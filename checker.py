4e685ba16277b333c058c6bd0acd9122a8529157
carlivoix <charlesvivoix@gmail.com>
2019-10-29 17:51:12 +0100

    the type of the elements of the solution is checked and too big elements as well

--- checker.py
+++ checker.py
@@ -1,8 +1,9 @@
 
 
 # Counts the number of cycles in the solution. solution[i] == i is counted as a cycle.
-# parameter: solution: a solution to the TSP
-# return: the number of cycles in the solution
+# parameter: solution: a solution to the TSP, the ith element of the list is the city visited after the ith one
+# return: the number of cycles in the solution or 10 if one of elements of the solution is not between 0 and the number
+# of cities -1
 def nb_cycles(solution):
     visited = [0] * len(solution)
     nb = 0
@@ -10,7 +11,12 @@ def nb_cycles(solution):
         j = i
         if visited[j] == 0:
             nb += 1
+        first_index = j
         while visited[j] < 1:
             visited[j] += 1
+            if solution[j] not in range(len(solution)):
+                return 10
             j = solution[j]
+        if j != first_index:
+            return 0
     return nb
--- checker_test.py
+++ checker_test.py
@@ -10,9 +10,18 @@ if c.nb_cycles([0, 1, 2]) == 1:
     nb_pb += 1
     print("Problem: the improper solution [0, 1, 2] was detected as proper")
 
-if c.nb_cycles([0, 1, 2, 1]):
+if c.nb_cycles([1, 2, 3, 1]) == 1:
     nb_pb += 1
-    print("Problem: the improper solution [0, 1, 2, 1] was detected as proper")
+    print("Problem: the improper solution [1, 2, 3, 1] was detected as proper")
+
+if c.nb_cycles([10, 2, 3, 0]) == 1:
+    nb_pb += 1
+    print("Problem: the improper solution [10, 2, 3, 0] was detected as proper")
+
+
+if c.nb_cycles(["c", 2, 3, 0]) == 1:
+    nb_pb += 1
+    print("Problem with a solution contating a non Integer element")
 
 if nb_pb == 0:
     print("No Problem was detected by the unit tests")
