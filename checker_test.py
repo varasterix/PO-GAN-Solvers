83029093d72ecd4efd770f33e6884152159760dc
carlivoix <charlesvivoix@gmail.com>
2019-10-29 15:51:33 +0100

    comments in english

--- checker_test.py
+++ checker_test.py
@@ -4,15 +4,15 @@ nb_pb = 0
 
 if c.nb_cycles([1, 2, 3, 0]) != 1:
     nb_pb += 1
-    print("Problème: le solution simple sans cycle [1, 2, 3, 0] a été détectée comme une mauvaise solution")
+    print("Problem: the proper solution [1, 2, 3, 0] was detected as a improper")
 
 if c.nb_cycles([0, 1, 2]) == 1:
     nb_pb += 1
-    print("Problème: la mauvaise solution [0, 1, 2] a été détectée comme une bonne solution")
+    print("Problem: the improper solution [0, 1, 2] was detected as proper")
 
 if c.nb_cycles([0, 1, 2, 1]):
     nb_pb += 1
-    print("Problème: la mauvaise solution [0, 1, 2, 1] a été détectée comme une bonne solution")
+    print("Problem: the improper solution [0, 1, 2, 1] was detected as proper")
 
 if nb_pb == 0:
-    print("Aucun problème détecté par les tests unitaires")
+    print("No Problem was detected by the unit tests")
