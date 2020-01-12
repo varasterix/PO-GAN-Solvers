06e29dc6af014a58d8caefbaf27bbaabdaf8d3b7
Emilie Lhomme <emilie.lhomme@laposte.net>
2020-01-12 14:10:46 +0100

    The 2-opt heuristic is bounded to tsp symmetric instances

--- src/objects/objectsTools.py
+++ src/objects/objectsTools.py
@@ -8,3 +8,23 @@ def is_weight_matrix_valid_structure(weight_matrix):
     """
     return (type(weight_matrix) == np.ndarray and weight_matrix.dtype == int and
             weight_matrix.shape[1] == weight_matrix.shape[0])
+
+
+def is_weight_matrix_symmetric(weight_matrix):
+    """
+    The structure of the weight matrix is valid if each element of the matrix (size n x n) is an integer
+    The weight matrix has also to be symmetric
+    :return: True if the structure of the weight matrix is valid and symmetric, False otherwise
+    """
+    if not is_weight_matrix_valid_structure(weight_matrix):
+        return False
+    else:
+        is_symmetric = True
+        n = len(weight_matrix)
+        for i in range(n):
+            for j in range(i):
+                if i != j:
+                    is_symmetric = is_symmetric and (weight_matrix[i, j] == weight_matrix[j, i])
+                if not is_symmetric:
+                    break
+    return is_symmetric
---
+++ test/objects/test_objectsTools.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+from src.objects.objectsTools import is_weight_matrix_symmetric
+
+weight_matrix_1 = np.array([[0, 5, 1, 10, 1],
+                            [1, 0, 1, 8, 1],
+                            [1, 15, 0, 4, 6],
+                            [8, 1, 2, 0, 2],
+                            [9, 7, 0, 1, 0]], dtype=int)
+weight_matrix_2 = np.array([[0, 4, 3, 1, 2],
+                            [4, 0, 7, 3, 2],
+                            [3, 7, 0, 4, 5],
+                            [1, 3, 4, 0, 1],
+                            [2, 2, 5, 1, 0]], dtype=int)
+
+
+class TestObjectToolsMethods(unittest.TestCase):
+    def test_is_weight_matrix_symmetric(self):
+        self.assertTrue(is_weight_matrix_symmetric(weight_matrix_2))
+        self.assertFalse(is_weight_matrix_symmetric(weight_matrix_1))
+
+
+if __name__ == '__main__':
+    unittest.main()
