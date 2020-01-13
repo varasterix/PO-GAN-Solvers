c35b63a26193bca2d985da2b2d96052659e087d4
Aymeric Varasse <43675676+varasterix@users.noreply.github.com>
2020-01-13 16:34:19 +0100

    Load dataset for training

--- src/gan/gan.py
+++ src/gan/gan.py
@@ -6,6 +6,9 @@ from src.discriminator.discriminator import Discriminator
 from src.discriminator.checker import nb_cycles
 from src.objects import neighboursBinaryMatrix, neighbours
 from src.activation.max_over_columns import max_over_columns
+from src.database import databaseTools
+from torch.utils.data import DataLoader
+import os
 
 
 class GAN:
@@ -20,7 +23,13 @@ class GAN:
         )
 
     def train(self, epochs=1000, batch_size=128):
-        weightmatrix = np.random.randint(0, 100, (10, 10))
+        # weightmatrix = np.random.randint(0, 100, (10, 10))
+
+        # configure data loader
+        os.makedirs("/test/tsp_database/", exist_ok=True)
+        dataloader = torch.utils.data.DataLoader(databaseTools.read_tsp_heuristic_solution_file(
+            10, [i for i in range(2000)]))
+
         for epoch in range(epochs):
             avg_loss = 0
             for iter in range(batch_size):
@@ -30,7 +39,7 @@ class GAN:
                     output = self.generator(input)
                     nbm = neighboursBinaryMatrix.NeighboursBinaryMatrix(np.array(output.detach(),
                                                                         dtype=int).reshape((10, 10)).transpose(),
-                                                                        weightmatrix)
+                                                                        dataloader)
                     nb_cycle_pred = torch.tensor([nbm.get_nb_cycles()], dtype=torch.float, requires_grad=True)
                     nb_cycle = torch.tensor([1], dtype=torch.float, requires_grad=True)
 
