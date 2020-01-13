8d57ed4acc688e9cad222d2454961e720696b313
Aymeric Varasse <43675676+varasterix@users.noreply.github.com>
2020-01-13 14:25:31 +0100

    Update dataset generation file

--- Java/TSP.java
+++ Java/TSP.java
@@ -1,5 +1,3 @@
-import java.io.*;
-
 public class TSP {
 
     public static int nb_cities;
@@ -8,10 +6,11 @@ public class TSP {
     public TSP(int nb_cities) {
         this.nb_cities = nb_cities;
         this.matrix = new int[this.nb_cities][this.nb_cities];
-        for (int i=0; i<this.nb_cities; i++) {
-            for (int j=0; j<nb_cities; j++) {
-                this.matrix[i][j] = (int)(Math.random() * 1000);
+        for (int i = 0; i < this.nb_cities; i++) {
+            for (int j = 0; j < nb_cities; j++) {
+                this.matrix[i][j] = (int) (Math.random() * 1000);
             }
+            this.matrix[i][i] = 0;
         }
     }
 
@@ -26,13 +25,15 @@ public class TSP {
     @Override
     public String toString() {
         String res = "";
-        for (int i=0; i<this.getMatrix().length; i++) {
-            for (int j=0; j<this.getMatrix().length; j++) {
-                res += this.getMatrix()[i][j] + "   ";
+        for (int i = 0; i < this.getMatrix().length - 1; i++) {
+            for (int j = 0; j < this.getMatrix().length; j++) {
+                res += this.getMatrix()[i][j] + "\t";
             }
             res += "\n";
         }
+        for (int j = 0; j < this.getMatrix().length; j++) {
+            res += this.getMatrix()[this.getNb_cities() - 1][j] + "\t";
+        }
         return res;
     }
-
 }
