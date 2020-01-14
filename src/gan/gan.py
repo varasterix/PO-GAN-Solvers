2a3bb2527f7fbad38e04807dab5e35714e057cbf
carlivoix <charlesvivoix@gmail.com>
2020-01-14 13:51:35 +0100

    start of gan

--- src/gan/gan.py
+++ src/gan/gan.py
@@ -3,10 +3,14 @@ import torch
 import torch.nn.functional as F
 from src.generator.generator import Generator
 from src.discriminator.discriminator import Discriminator
-from src.discriminator.checker import nb_cycles
-from src.objects import neighboursBinaryMatrix, neighbours
-from src.activation.max_over_columns import max_over_columns
 from src.database import databaseTools
+import random
+
+
+def shuffle_list(*ls):
+    l = list(zip(*ls))
+    random.shuffle(l)
+    return zip(*l)
 
 
 class GAN:
@@ -16,9 +20,7 @@ class GAN:
         self.generator = Generator()
         self.discriminator = Discriminator()
 
-        self.loss_function = torch.nn.CrossEntropyLoss(
-
-        )
+        self.loss_function = torch.nn.MSELoss()
 
     def train(self, epochs=1000, batch_size=100):
         dataset = []
@@ -27,10 +29,57 @@ class GAN:
 
         for epoch in range(epochs):
             wm = []  # weight matrix
-            can = []  # candidate
-            for j in range(20):
-                wm += [dataset[k][0].get_weight_matrix() for k in range(j*batch_size, (j+1)*batch_size)]
-                can += [dataset[k][0].get_candidate() for k in range(j*batch_size, (j+1)*batch_size)]
+            can_solver = []  # candidate
+            can_gen = []
+            labels = []
+            for j in range(2000):
+
+                # Discriminator training
+                self.discriminator.optimizer.zero_grad()
+
+                wm = dataset[j][0].get_weight_matrix().reshape(100)
+                wm = torch.tensor(wm, dtype=torch.float, requires_grad=True)
+
+                can_solver = dataset[j][0].get_candidate()
+                binary_can_solver = [0 for k in range(100)]
+                for k in range(len(can_solver)):
+                    binary_can_solver[k*10+can_solver[k]] = 1
+                label = [0]
+
+                input_d = torch.tensor(binary_can_solver, dtype=torch.float, requires_grad=True)
+                output_d = torch.tensor(label, dtype=torch.float, requires_grad=True)
+                predicted_output_d = self.discriminator(input_d)
+
+                d_loss = self.loss_function(output_d, predicted_output_d)
+
+                d_loss.backward()
+                self.discriminator.optimizer.step()
+
+                self.discriminator.optimizer.zero_grad()
+
+                can_gen = self.generator(wm)
+                label = [1]
+
+                input_d = torch.tensor(can_gen, dtype=torch.float, requires_grad=True)
+                output_d = torch.tensor(label, dtype=torch.float, requires_grad=True)
+                predicted_output_d = self.discriminator(input_d)
+
+                d_loss = self.loss_function(output_d, predicted_output_d)
+
+                d_loss.backward()
+                self.discriminator.optimizer.step()
+
+                # Generator training
+                self.generator.optimizer.zero_grad()
+                input_g = can_gen
+                input_g = torch.tensor(input_g, dtype=torch.float, requires_grad=True)
+                output_g = torch.tensor(label, dtype=torch.float, requires_grad=True)
+                predicted_output_g = self.discriminator(input_g)
+
+                g_loss = self.loss_function(output_g, predicted_output_g)
+
+                g_loss.backward()
+                self.generator.optimizer().step()
 
 
 gan = GAN()
--- src/generator/generator.py
+++ src/generator/generator.py
@@ -1,7 +1,6 @@
 import torch.nn.functional as F
 import torch.nn as nn
 import torch
-from src.activation.max_over_columns import max_over_columns
 
 
 # class of the model
@@ -9,9 +8,9 @@ class Generator(nn.Module):
     # the definition of the layers
     def __init__(self):
         super(Generator, self).__init__()
-        self.fc1 = nn.Linear(1, 12)
-        self.fc2 = nn.Linear(12, 64)
-        self.fc3 = nn.Linear(64, 100)
+        self.fc1 = nn.Linear(100, 200)
+        self.fc2 = nn.Linear(200, 200)
+        self.fc3 = nn.Linear(200, 100)
 
         params = self.parameters()
         self.optimizer = torch.optim.SGD(params, lr=0.001)
@@ -20,5 +19,5 @@ class Generator(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = max_over_columns(self.fc3(x))
+        x = F.softmax(torch.reshape(self.fc3(x), (10, 10)))
         return x
