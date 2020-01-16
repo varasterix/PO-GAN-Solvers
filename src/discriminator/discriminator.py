a1f4b42264cb20b470419fd3612223c1432b812c
carlivoix <charlesvivoix@gmail.com>
2020-01-16 17:28:26 +0100

    debug gan

--- src/discriminator/discriminator.py
+++ src/discriminator/discriminator.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
-        self.fc1 = nn.Linear(200, 200)
+        self.fc1 = nn.Linear(100, 200)
         self.fc2 = nn.Linear(200, 200)
         self.fc3 = nn.Linear(200, 1)
 
@@ -16,6 +16,6 @@ class Discriminator(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.sigmoid(self.fc3(x))
+        x = torch.sigmoid(self.fc3(x))
         return x
 
