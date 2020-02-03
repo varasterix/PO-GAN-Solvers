2031b965772da73adce1f81419e976bb7a76c991
Aymeric Varasse <43675676+varasterix@users.noreply.github.com>
2020-02-03 10:38:07 +0100

    Fix batches implementation

--- src/gan/gan.py
+++ src/gan/gan.py
@@ -31,27 +31,40 @@ for epoch in range(epochs):
     batch = dataset[:]
     for data in batch:
 
-        # Discriminator training
-        d_optimizer.zero_grad()
-
         wm = data[0].get_weight_matrix().reshape(100)
         wm = torch.tensor(wm, dtype=torch.float, requires_grad=True)
-
         can_solver = data[0].get_candidate()
         binary_can_solver = [0 for k in range(100)]
+        can_gen = generator(wm)
+
+        # Generator training
+        label = [0]
+        g_optimizer.zero_grad()
+        input_g = can_gen
+        output_g = torch.tensor(label, dtype=torch.float, requires_grad=False)
+        predicted_output_g = discriminator(input_g)
+
+        g_loss = F.binary_cross_entropy(predicted_output_g, output_g)
+        avg_g_loss += g_loss.item()
+
+        g_loss.backward()
+        g_optimizer.step()
+
+        # Discriminator training
+        # d_optimizer.zero_grad()
+
         for k in range(len(can_solver)):
-            binary_can_solver[k*10+can_solver[k]] = 1
+            binary_can_solver[k * 10 + can_solver[k]] = 1
         label = [0]
         binary_can_solver = torch.tensor(binary_can_solver, dtype=torch.float, requires_grad=True)
+
+        can_gen = generator(wm)
+        label = [1]
 
         input_d = binary_can_solver
         output_d = torch.tensor(label, dtype=torch.float, requires_grad=False)
         predicted_output_d = discriminator(input_d)
-
         valid_d_loss = F.binary_cross_entropy(predicted_output_d, output_d)
-
-        can_gen = generator(wm)
-        label = [1]
 
         input_d = can_gen
         output_d = torch.tensor(label, dtype=torch.float, requires_grad=False)
@@ -61,22 +74,9 @@ for epoch in range(epochs):
         d_loss = (valid_d_loss + fake_d_loss) / 2
         avg_d_loss += d_loss.item()
 
-        d_loss.backward()
-        d_optimizer.step()
+        # d_loss.backward()
+        # d_optimizer.step()
 
-        # Generator training
-        label = [0]
-        g_optimizer.zero_grad()
-        input_g = can_gen
-        output_g = torch.tensor(label, dtype=torch.float, requires_grad=False)
-        predicted_output_g = discriminator(torch.reshape(torch.tensor(F.one_hot(torch.argmax(
-            torch.reshape(input_g.detach(), (10, 10)), dim=1), num_classes=10), dtype=torch.float), (100,)))
-
-        g_loss = F.binary_cross_entropy(predicted_output_g, output_g)
-        avg_g_loss += g_loss.item()
-
-        g_loss.backward()
-        g_optimizer.step()
     avg_d_loss /= 2000
     avg_g_loss /= 2000
     print("epoch: " + str(epoch + 1) + ": average loss of generator: " + str(avg_g_loss))
