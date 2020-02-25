92a182703d21e086353a433bb03826dce4482820
carlivoix <charlesvivoix@gmail.com>
2020-02-25 13:01:39 +0100

    parameter tuning

--- src/discriminator/discriminator.py
+++ src/discriminator/discriminator.py
@@ -7,8 +7,9 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.fc1 = nn.Linear(100, 200)
-        self.fc2 = nn.Linear(200, 200)
-        self.fc3 = nn.Linear(200, 1)
+        self.fc2 = nn.Linear(200, 500)
+        self.fc3 = nn.Linear(500, 200)
+        self.fc4 = nn.Linear(200, 1)
 
         params = self.parameters()
         self.optimizer = torch.optim.SGD(params, lr=0.001)
@@ -16,6 +17,7 @@ class Discriminator(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
+        x = torch.relu(self.fc3(x))
+        x = torch.sigmoid(self.fc4(x))
         return x
 
