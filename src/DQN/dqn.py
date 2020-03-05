d48aeaf3e902fd04c11b836f724b228ab49603bb
Aymeric Varasse <43675676+varasterix@users.noreply.github.com>
2020-03-05 10:26:39 +0100

    Update network structure New dataset files

--- src/DQN/dqn.py
+++ src/DQN/dqn.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+# import torch.nn.functional as F
 
 p = 0.9  # dropout
 
@@ -9,15 +9,17 @@ class DQN(nn.module):
 
     def __init__(self, weight_matrix):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(100, 200)
-        self.bn1 = nn.BatchNorm1d(200)
-        self.fc2 = nn.Linear(200, 200)
-        self.bn2 = nn.BatchNorm1d(200)
-        self.fc3 = nn.Linear(200, 10)
-        self.bn3 = nn.BatchNorm1d(10)
+        matrix_length = len(weight_matrix)
+        matrix_size = matrix_length**2
+        self.fc1 = nn.Linear(matrix_size, 2*matrix_size)
+        self.bn1 = nn.BatchNorm1d(2*matrix_size)
+        self.fc2 = nn.Linear(2*matrix_size, 2*matrix_size)
+        self.bn2 = nn.BatchNorm1d(2*matrix_size)
+        self.fc3 = nn.Linear(2*matrix_size, matrix_length)
+        self.bn3 = nn.BatchNorm1d(matrix_length)
 
     def forward(self, x):
-        model = torch.nn.Sequential(self.fc1, self.bn1, F.dropout(p), F.relu(),
-                                    self.fc2, self.bn2, F.dropout(p), F.relu(),
-                                    self.fc3, self.bn3, F.dropout(p), F.relu())
+        model = torch.nn.Sequential(self.fc1, self.bn1, nn.Dropout(p), nn.ReLU(),
+                                    self.fc2, self.bn2, nn.Dropout(p), nn.ReLU(),
+                                    self.fc3, self.bn3, nn.Dropout(p), nn.ReLU())
         return model(x)
