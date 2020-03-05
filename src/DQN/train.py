a0534b6f60e1eee85730d7cceffa919fde267e48
varasse <43675676+varasse@users.noreply.github.com>
2020-03-05 11:06:37 +0100

    Style formatting

--- src/DQN/train.py
+++ src/DQN/train.py
@@ -47,8 +47,7 @@ steps_done = 0
 def select_action(state):
     global steps_done
     sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-                    math.exp(-1. * steps_done / EPS_DECAY)
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
