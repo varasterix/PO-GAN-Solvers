4f62fd7fcd2f559ca4c00ee1ec459822c0cb0e58
varasse <43675676+varasse@users.noreply.github.com>
2020-03-05 11:03:30 +0100

    Style formatting

--- src/DQN/dqn.py
+++ src/DQN/dqn.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+
 # import torch.nn.functional as F
 
 p = 0.9  # dropout
@@ -10,12 +11,12 @@ class DQN(nn.module):
     def __init__(self, weight_matrix):
         super(DQN, self).__init__()
         matrix_length = len(weight_matrix)
-        matrix_size = matrix_length**2
-        self.fc1 = nn.Linear(matrix_size, 2*matrix_size)
-        self.bn1 = nn.BatchNorm1d(2*matrix_size)
-        self.fc2 = nn.Linear(2*matrix_size, 2*matrix_size)
-        self.bn2 = nn.BatchNorm1d(2*matrix_size)
-        self.fc3 = nn.Linear(2*matrix_size, matrix_length)
+        matrix_size = matrix_length ** 2
+        self.fc1 = nn.Linear(matrix_size, 2 * matrix_size)
+        self.bn1 = nn.BatchNorm1d(2 * matrix_size)
+        self.fc2 = nn.Linear(2 * matrix_size, 2 * matrix_size)
+        self.bn2 = nn.BatchNorm1d(2 * matrix_size)
+        self.fc3 = nn.Linear(2 * matrix_size, matrix_length)
         self.bn3 = nn.BatchNorm1d(matrix_length)
 
     def forward(self, x):
