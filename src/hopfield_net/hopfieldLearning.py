490bdd6036b9dfb6406b8e75cce6e7a9e535e802
Pancake39 <emilie.lhomme@laposte.net>
2020-03-12 13:57:28 +0100

    Some modifications are added in src/hopfield_net/ files

--- src/hopfield_net/hopfieldLearning.py
+++ src/hopfield_net/hopfieldLearning.py
@@ -1,17 +1,20 @@
 import os
 import random
+import time
 import matplotlib.pyplot as plt
 from src.database.databaseTools import read_tsp_choco_solution_file
 from src.hopfield_net.hopfield_TSP_network import HopfieldTSPNetwork
 import src.constants as constants
+from src.utils import plot_tsp_candidates
 
 
 if __name__ == '__main__':
     # Parameters
     tsp_database_path = "../../" + constants.PARAMETER_TSP_CHOCO_DATA_FILES
-    number_of_examples = 5
-    A, B, C, D = 5000, 5000, 5000, 0
-    MAX_ITERATIONS = 500
+    number_of_examples = 1
+    number_of_restarts = 10
+    A, B, C, D = 5000, 5000, 2000, 0.8
+    MAX_ITERATIONS = 100
     parameters = {'A': A, 'B': B, 'C': C, 'D': D}
 
     # Loading the chosen TSP dataSet
@@ -22,9 +25,10 @@ if __name__ == '__main__':
 
     # Experimentation on the TSP dataSet
     print("Beginning of the experimentation...")
-    number_of_iterations = 0
-    number_of_solutions = 0
-    relative_gap = 0
+    sum_iterations = 0
+    sum_solutions = 0
+    sum_solving_times = 0
+    sum_relative_gaps = 0
     for data_file in tsp_database_files[:number_of_examples]:
         # Loading one TSP dataSet
         details = data_file.split('.')[0].split('_')
@@ -33,24 +37,38 @@ if __name__ == '__main__':
         ordered_path, total_weight = read_tsp_choco_solution_file(nb_cities, instance_id, tsp_database_path)
         weight_matrix = ordered_path.get_weight_matrix()
         cartesian_coordinates = ordered_path.get_cartesian_coordinates()
-        # Creation of Hopfield TSP Network Model
-        network = HopfieldTSPNetwork(nb_cities=nb_cities, distance_matrix=weight_matrix,
-                                     cartesian_coordinates=cartesian_coordinates, penalty_parameters=parameters)
-        iteration, energy_by_iterations = network.run_until_stable(max_iterations=MAX_ITERATIONS)
-        candidate = network.get_ordered_path_binary_matrix()
-        if candidate.is_solution():
+
+        # Creation of + Running the Hopfield TSP Network Model
+        start_time = time.time()
+        best_candidate = None
+        for i in range(number_of_restarts):
+            print('...Restart {}'.format(i + 1))
+            network = HopfieldTSPNetwork(nb_cities=nb_cities, distance_matrix=weight_matrix,
+                                         cartesian_coordinates=cartesian_coordinates, penalty_parameters=parameters)
+            iteration, energy_by_iterations = network.run_until_stable(max_iterations=MAX_ITERATIONS,
+                                                                       stop_at_local_min=False)
+            candidate = network.get_best_ordered_path_binary_matrix()
+            if candidate.is_solution():
+                if best_candidate is None or best_candidate.distance() > candidate.distance():
+                    print(str(candidate.distance()))
+                    best_candidate = candidate.__copy__()
+            sum_iterations += iteration
+            # plt.figure()
+            # plt.plot(range(len(energy_by_iterations)), energy_by_iterations)
+            # plt.show()
+        if best_candidate is not None:
             print("-> Solution found!")
-            number_of_solutions += 1
-            relative_gap += (candidate.distance() - ordered_path.distance()) / ordered_path.distance()
-        number_of_iterations += iteration
-        # plt.figure()
-        # plt.plot(range(len(energy_by_iterations)), energy_by_iterations)
-        # plt.show()
-    number_of_solutions /= number_of_examples
-    number_of_iterations /= number_of_examples
-    relative_gap /= number_of_examples
+            sum_solutions += 1
+            sum_relative_gaps += (best_candidate.distance() - ordered_path.distance()) / ordered_path.distance()
+            plot_tsp_candidates([ordered_path, best_candidate])
+        sum_solving_times = time.time() - start_time
+    sum_solutions /= number_of_examples
+    sum_iterations /= number_of_examples * number_of_restarts
+    sum_solving_times /= number_of_examples
+    sum_relative_gaps /= number_of_examples
     print("Experimentation finished!")
     print("Report of the experimentation:")
-    print("Percentage of solutions found : {:.1f}%, Mean number of iterations : {:.2f}"
-          .format(number_of_solutions * 100, number_of_iterations))
-    print("For the solutions found, Mean of the relative gap with Choco solutions : {:.4f}".format(relative_gap))
+    print("Percentage of solutions found : {:.1f}%, Mean number of iterations : {:.2f} in {:.2f} seconds"
+          .format(sum_solutions * 100, sum_iterations, sum_solving_times))
+    print("For the solutions found, Mean of the relative gap with Choco solutions : {:.2f}%"
+          .format(sum_relative_gaps * 100))
