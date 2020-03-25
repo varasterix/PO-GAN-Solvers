0a80c6b1429c969c1ee5f42087a1a7409550a32f
Pancake39 <emilie.lhomme@laposte.net>
2020-03-25 15:32:29 +0100

    hopfield Learning is modified + some results are added + src/utils.py modified

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import math
 import matplotlib.pyplot as plt
 from src.objects import candidateTSP as cTSP
 
@@ -7,7 +8,8 @@ def custom_formatwarning(msg, *args, **kwargs):
     return str(msg) + '\n'
 
 
-def plot_tsp_candidates(candidates):
+def plot_tsp_candidates(candidates, instance_id, label_titles=None, save_path=None, show_figure=False):
+    # Exceptions control
     cartesian_coordinates_ref = None
     ordered_paths = []
     for i, candidate in enumerate(candidates):
@@ -25,21 +27,32 @@ def plot_tsp_candidates(candidates):
     for oP in ordered_paths:
         if not oP.is_valid_structure():
             raise Exception('One of the given candidates has not a valid structure')
+    nb_ordered_paths = len(ordered_paths)
+    sqrt_ordered_paths = math.ceil(math.sqrt(nb_ordered_paths))
     annotation_gap = 10
-    # plt.figure("TSP candidate figure")
-    fig, axs = plt.subplots(1, len(ordered_paths))
-    plt.suptitle("TSP candidates - Representation of the cycle")
+    fig, axs = plt.subplots(nb_ordered_paths // sqrt_ordered_paths, sqrt_ordered_paths)
+    plt.suptitle("TSP candidates instance {} - Representation of the cycles".format(instance_id))
     for i, oP in enumerate(ordered_paths):
-        for j, (x, y) in enumerate(ordered_paths[i].get_cartesian_coordinates()):
-            axs[i].plot(x, y, "ok")
-            axs[i].annotate(j, (x + annotation_gap, y + annotation_gap))
-        label = "Not a TSP solution" if not oP.is_solution() else "Solution, D=" + str(oP.distance())
+        ax = axs[i] if (nb_ordered_paths // sqrt_ordered_paths <= 1) \
+            else axs[i // sqrt_ordered_paths, i % sqrt_ordered_paths]
+        for j, (x, y) in enumerate(oP.get_cartesian_coordinates()):
+            ax.plot(x, y, 'ok')
+            ax.annotate(j, (x + annotation_gap, y + annotation_gap))
+        label = "Not TSP solution" if not oP.is_solution() else "D=" + str(oP.distance())
         x_seq, y_seq = [], []
         for city in oP.get_candidate():
             x_seq.append(oP.get_cartesian_coordinates()[city, 0])
             y_seq.append(oP.get_cartesian_coordinates()[city, 1])
         x_seq.append(oP.get_cartesian_coordinates()[oP.get_candidate()[0], 0])
         y_seq.append(oP.get_cartesian_coordinates()[oP.get_candidate()[0], 1])
-        axs[i].plot(x_seq, y_seq, label=label)
-        axs[i].legend()
-    plt.show()
+        ax.plot(x_seq, y_seq, label=label)
+        ax.set_title('' if label_titles is None else label_titles[i])
+        ax.legend()
+    # Hide x labels and tick labels for top plots and y ticks for right plots.
+    for ax in axs.flat:
+        ax.label_outer()
+
+    if save_path is not None:
+        plt.savefig(save_path)
+    if show_figure:
+        plt.show()
